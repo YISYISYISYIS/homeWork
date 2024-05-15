05b88a95d87c4107771627ed420c410d047f87e2
YIS <dtdt203@naver.com>
2024-05-15 22:43:10 +0900

    style수정후 완료

--- reactTask1/src/App.jsx
+++ reactTask1/src/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
     setTitle("");
     setContent("");
   };
+  // //card 추가
 
   const ChangeCard = (id)=>{
     setCards(Cards.map((card)=>{
@@ -32,52 +33,79 @@ const App = () => {
      }
     }));
   };
+  // //card 상태변경
 
   const removeCard = (id)=>{
     setCards(Cards.filter((card)=>{
       return card.id !== id;
     }));
   };
+  // //card 삭제
 
 
 
 
   return (
     <div className='app'>
-      App
-      <form onSubmit={addCard}>
-        <input type="text" placeholder='제목' value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
-        <input type="text" placeholder='내용' value={content} onChange={(e)=>{setContent(e.target.value)}}/>
+      <div className="header" style={{display: "flex", justifyContent: "space-between", alignItems:"center",padding:"0 2rem" ,height: "5vh" }}>
+        <a href="#"><h1 style={{fontSize: "1.5rem"}}>My Todo List</h1></a>
+        <p>React</p>
+      </div>
+      {/* //header */}
+
+      <form className="input" onSubmit={addCard} style={{display: "flex",justifyContent: "space-between",height: "10vh", padding:"0 2rem", alignItems: "center", backgroundColor: "#e3e3e3"}}>
+        <div style={{display:"flex",alignItems:"center", height:"2rem"}}>
+          <h3 style={{fontSize:"1rem"}}>제목</h3>
+          <input type="text" style={{ marginLeft:"1rem", height:"2rem", borderRadius: ".5rem", width:"18rem"}} placeholder='제목' value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+        </div>
+        <div style={{display:"flex",alignItems:"center", height:"2rem", marginLeft:"-16rem"}}>
+          <h3 style={{fontSize:"1rem"}}>내용</h3>
+          <input type="text" style={{ marginLeft:"1rem", height:"2rem", borderRadius: ".5rem", width:"18rem"}} placeholder='내용' value={content} onChange={(e)=>{setContent(e.target.value)}}/>
+        </div>
         <button type='submit'>카드 추가</button>
       </form>
-      <h1>working</h1>
-      <div style={{display: "flex", flexWrap: "wrap"}}>
-        {Cards.filter((card)=>(card.isDone === false)).map((card)=>(
-          <div key = {card.id} style ={{display: "flex",flexDirection: "column", alignItems: "center",border: "1px solid #000", width: "300px", height: "200px", margin: "1rem"}}>
-            <h2>{card.title}</h2>
-            <p>{card.content}</p>
-            <div style={{display: "flex"}}>
-              <button onClick={()=>{removeCard(card.id)}}>삭제하기</button>
-              <button onClick={()=>{ChangeCard(card.id)}}>완료</button>
-            </div>
+      {/* //input */}
+
+      <div className="main">
+        <div className='working'>
+          <div style={{display:"flex",alignItems:"center", marginLeft:"2rem", height: "8vh"}}>
+            <h2>working</h2>
           </div>
-        ))}
-      </div>
-      <div>
-        <h1>Done</h1>
-        <div style={{display: "flex", flexWrap: "wrap"}}>
-        {Cards.filter((card)=>(card.isDone === true)).map((card)=>(
-          <div key = {card.id} style ={{display: "flex",flexDirection: "column", alignItems: "center",border: "1px solid #000", width: "300px", height: "200px", margin: "1rem"}}>
-            <h2>{card.title}</h2>
-            <p>{card.content}</p>
-            <div style={{display: "flex"}}>
-              <button onClick={()=>{removeCard(card.id)}}>삭제하기</button>
-              <button onClick={()=>{ChangeCard(card.id)}}>취소</button>
-            </div>
+          <div style={{display: "flex", flexWrap: "wrap"}}>
+            {Cards.filter((card)=>(card.isDone === false)).map((card)=>(
+              <div key = {card.id} style ={{display: "flex",flexDirection: "column",justifyContent:"space-around" ,alignItems: "center",border: "1px solid #000", width: "21rem", height: "10rem", margin: "0rem 2rem 2rem", padding:"1rem", borderRadius:"0.5rem"}}>
+                <h2 style={{alignSelf: "flex-start"}}>{card.title}</h2>
+                <p style={{alignSelf: "flex-start"}}>{card.content}</p>
+                <div style={{display: "flex", justifyContent:"center"}}>
+                  <button onClick={()=>{removeCard(card.id)}}>삭제하기</button>
+                  <button style={{marginLeft:".5rem"}} onClick={()=>{ChangeCard(card.id)}}>완료</button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
+        {/* //working */}
+
+        <div className='done'>
+          <div style={{display:"flex",alignItems:"center", marginLeft:"2rem", height: "8vh"}}>
+            <h2>Done</h2>
+          </div>
+          <div style={{display: "flex", flexWrap: "wrap"}}>
+          {Cards.filter((card)=>(card.isDone === true)).map((card)=>(
+            <div key = {card.id} style ={{display: "flex",flexDirection: "column",justifyContent:"space-around" ,alignItems: "center",border: "1px solid #000", width: "21rem", height: "10rem", margin: "0rem 2rem 2rem", padding:"1rem", borderRadius:"0.5rem"}}>
+              <h2 style={{alignSelf: "flex-start"}}>{card.title}</h2>
+              <p style={{alignSelf: "flex-start"}}>{card.content}</p>
+              <div style={{display: "flex"}}>
+                <button onClick={()=>{removeCard(card.id)}}>삭제하기</button>
+                <button style={{marginLeft:"0.5rem"}} onClick={()=>{ChangeCard(card.id)}}>취소</button>
+              </div>
+            </div>
+          ))}
+          </div>
+        </div>
+        {/* //done */}
       </div>
-      </div>
+      {/* //main */}
     </div>
   )
 }
